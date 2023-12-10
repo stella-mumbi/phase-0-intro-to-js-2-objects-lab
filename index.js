092794653c4055cdf5c01acb68f70b17ddebef60
stella-mumbi <stella.mumbi@student.moringaschool.com>
2023-12-10 15:08:23 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+var  employee = {
+    name: "Jane",
+    streetAddress: "123 Main St"
+  };
+
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+
+    return {
+      ...employee,
+      [key]: value
+    };
+  }
+
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+
+
+       const updatedemployee=[key] .value
+    return updatedEmployee;
+        
+      };
+
+
+  function deleteFromEmployeeByKey(employee, key) {
+    const updatedEmployee = { ...employee };
+    delete updatedEmployee[key];
+
+    return updatedEmployee;
+  }
+
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+
+    return employee[key];
+  }
