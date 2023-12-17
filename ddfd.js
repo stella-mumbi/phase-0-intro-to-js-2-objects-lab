328b2df91f5fe11de7c842ff50ee23216a774b5f
stella-mumbi <stella.mumbi@student.moringaschool.com>
2023-12-17 14:48:50 +0300

    completed assignment

---
+++ ddfd.js
@@ -0,0 +1,31 @@
+// Write your solution in this file!
+var  employee = {
+    name: "Jane",
+    streetAddress: "123 Main St"
+  };
+
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+   
+    return {
+      ...employee,
+      [key]: value
+    };
+  }
+
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+   
+
+  function deleteFromEmployeeByKey(employee, key) {
+    const updatedEmployee = { ...employee };
+    delete updatedEmployee[key];
+  
+    return updatedEmployee;
+  }
+
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
--- index.js
+++ index.js
@@ -1,34 +1,31 @@
 // Write your solution in this file!
 var  employee = {
-    name: "Jane",
-    streetAddress: "123 Main St"
+  name: "Jane",
+  streetAddress: "123 Main St"
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+ 
+  return {
+    ...employee,
+    [key]: value
   };
+}
 
-  function updateEmployeeWithKeyAndValue(employee, key, value) {
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+ 
 
-    return {
-      ...employee,
-      [key]: value
-    };
-  }
+function deleteFromEmployeeByKey(employee, key) {
+  const updatedEmployee = { ...employee };
+  delete updatedEmployee[key];
 
-  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  return updatedEmployee;
+}
 
-
-       const updatedemployee=[key] .value
-    return updatedEmployee;
-        
-      };
-
-
-  function deleteFromEmployeeByKey(employee, key) {
-    const updatedEmployee = { ...employee };
-    delete updatedEmployee[key];
-
-    return updatedEmployee;
-  }
-
-  function destructivelyDeleteFromEmployeeByKey(employee, key) {
-
-    return employee[key];
-  }
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
